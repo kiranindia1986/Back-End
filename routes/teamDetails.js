15a6fcb62431210b125213e895b285ddc9faaa1d
Kiran <kiranindia1986@gmail.com>
2025-02-13 00:18:33 +0530

    push

--- routes/teamDetails.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getTeamDetails } = require('../controllers/teamDetailsController');
-const router = express.Router();
-
-// Route to fetch team details for a user
-router.get('/:uid', getTeamDetails);
-
-module.exports = router;
